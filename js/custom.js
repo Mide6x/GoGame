35973dee402ba772bed413f8897554aad80d87b6
Olumide Adewole <89521100+Mide6x@users.noreply.github.com>
2021-10-04 15:13:45 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,11 @@
 
 window.onload = function() {
-  $('.block-row').parallax();
+  $('.block-row').parallax(); 
+  
+$('.cookie-banner').delay(2000).fadeIn();
+  localStorage.setItem('cookieSeen','shown')
 };
+
+$('.close').click(function() {
+  $('.cookie-banner').fadeOut();
+})
