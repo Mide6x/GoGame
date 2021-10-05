154c24b7206b4570f7f1a5c3b5390e6d24129aa3
Olumide Adewole <89521100+Mide6x@users.noreply.github.com>
2021-10-05 15:41:47 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-
+{
 window.onload = function() {
   $('.block-row').parallax();
   
@@ -7,7 +7,35 @@ window.onload = function() {
 };
 
 
-
 $('.close').click(function() {
   $('.cookie-banner').fadeOut();
 })
+
+ /*fadein features*/
+
+ const appearOptions = {
+   threshold: 1,
+   rootMargin: "0px 0px -80px 0px"
+ };
+
+ const faders = document.querySelectorAll('.feature_box');
+ const appearOnScroll = new IntersectionObserver (function (
+   entries,
+   appearOnScroll
+   ) {
+     entries.forEach(entry => {
+       if (!entry.isIntersecting) {
+         return;
+       } else {
+         entry.target.classList.add('appear');
+         appearOnScroll.unobserve(entry.target);
+       }
+     })
+   }, 
+   appearOptions);
+
+   faders .forEach(fader => {
+     appearOnScroll.observe(fader);
+   });
+
+}
